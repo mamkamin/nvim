27f105f99a900da784014b543605f16de36e9c85
Muhammad Amin <muhdaminkafri45@gmail.com>
2026-08-08 10:06:28 +0800

    keymap: leader pv to go to Ex

--- init.lua
+++ init.lua
@@ -46,6 +46,7 @@ vim.o.confirm = true
 
 -- Use <Esc> to exit terminal mode
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- AUTOCOMMANDS (EVENT HANDLERS)
 --
