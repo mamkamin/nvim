258946db8877ff0d0387db22c4cb5234bc2b2f3d
Muhammad Amin <muhdaminkafri45@gmail.com>
2026-08-10 19:52:18 +0800

    add keymaps to use cnext and cprev

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set('n', '<M-j>', vim.cmd.cnext)
+vim.keymap.set('n', '<M-k>', vim.cmd.cprev)
